f8f209ba773fc52fccb861192fe9c1c4afe560d4
Emerson Junqueira <emoonlit@gmail.com>
2024-06-09 20:08:06 -0300

    feat: added lb4 jwt authentication

--- src/application.ts
+++ src/application.ts
@@ -1,13 +1,16 @@
+import {AuthenticationComponent} from '@loopback/authentication';
+import {JWTAuthenticationComponent, UserServiceBindings} from '@loopback/authentication-jwt';
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
 import {
   RestExplorerBindings,
   RestExplorerComponent,
 } from '@loopback/rest-explorer';
-import {RepositoryMixin} from '@loopback/repository';
-import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
+import {Lb4TodoDbDataSource} from './datasources';
 import {MySequence} from './sequence';
 
 export {ApplicationConfig};
@@ -28,6 +31,7 @@ export class TodoListApplication extends BootMixin(
     this.configure(RestExplorerBindings.COMPONENT).to({
       path: '/explorer',
     });
+
     this.component(RestExplorerComponent);
 
     this.projectRoot = __dirname;
@@ -40,5 +44,14 @@ export class TodoListApplication extends BootMixin(
         nested: true,
       },
     };
+
+    // Mount authentication system
+    this.component(AuthenticationComponent);
+
+    // Mount jwt component
+    this.component(JWTAuthenticationComponent);
+
+    // Bind datasource
+    this.dataSource(Lb4TodoDbDataSource, UserServiceBindings.DATASOURCE_NAME)
   }
 }
